51419de24a8c0867d8b1706886e7238a1892daf7
ShukurulloGanisherov2012 <162418164+ShukurulloGanisherov2012@users.noreply.github.com>
2024-03-07 19:02:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const change = () => {
+    var img = document.getElementById("demo2");
+    if(img.src == "https://robots.net/wp-content/uploads/2023/10/why-is-my-tablet-screen-black-1696681441.jpg"){
+        img.src = "https://hdclipartall.com/images/tablet-clipart-tablet-black-screen-png-image-and-clipart-650.jpg";
+    }else if(img.src == "https://hdclipartall.com/images/tablet-clipart-tablet-black-screen-png-image-and-clipart-650.jpg"){
+        img.src = "https://images-eu.ssl-images-amazon.com/images/I/51nrgs5yLIL._AC_UL600_SR600,600_.jpg";
+    }else if(img.src == "https://images-eu.ssl-images-amazon.com/images/I/51nrgs5yLIL._AC_UL600_SR600,600_.jpg"){
+        img.src = "https://media.product.which.co.uk/prod/images/original/eaa29035f160-ic17227-0882-00-rear-2000x1500.webp";
+    }else if(img.src == "https://media.product.which.co.uk/prod/images/original/eaa29035f160-ic17227-0882-00-rear-2000x1500.webp"){
+        img.src = "https://www.powerbuy.co.th/_next/image?url=https%3A%2F%2Fpim.powerbuy.co.th%2FPWBPIM%2Fweb%2FThumbnail%2FImage%2F1207%2F276670.jpg&w=640&q=75";
+    }else if(img.src == "https://www.powerbuy.co.th/_next/image?url=https%3A%2F%2Fpim.powerbuy.co.th%2FPWBPIM%2Fweb%2FThumbnail%2FImage%2F1207%2F276670.jpg&w=640&q=75"){
+        img.src = "https://isupplyfiji.com/cdn/shop/products/86431.1_1024x1024.webp?v=1682288793";
+    }else if(img.src == "https://isupplyfiji.com/cdn/shop/products/86431.1_1024x1024.webp?v=1682288793"){
+        img.src = "https://m.media-amazon.com/images/I/61+JCBk9tdL.jpg";
+    }else if(img.src == "https://m.media-amazon.com/images/I/61+JCBk9tdL.jpg"){
+        img.src = "https://www.discoazul.com/uploads/media/images/tablet-alcatel-3t10-2020-10-1-2gb-32gb-4g-negra-14.jpg";
+    }else if(img.src == "https://www.discoazul.com/uploads/media/images/tablet-alcatel-3t10-2020-10-1-2gb-32gb-4g-negra-14.jpg"){
+        img.src = "https://media.s-bol.com/qLZQZpqLRnRp/nRY9RXP/168x115.jpg";
+    }else if(img.src == "https://media.s-bol.com/qLZQZpqLRnRp/nRY9RXP/168x115.jpg"){
+        img.src = "https://image-us.samsung.com/SamsungUS/home/mobile/tablets/galaxy-tab-s7-fe/GalaxyTabS7plusLite_Combo_001_MysticGreen_1600x1200.png?$product-details-jpg$";
+    }else if(img.src == "https://image-us.samsung.com/SamsungUS/home/mobile/tablets/galaxy-tab-s7-fe/GalaxyTabS7plusLite_Combo_001_MysticGreen_1600x1200.png?$product-details-jpg$"){
+        img.src = "https://m.media-amazon.com/images/I/71g9X7W9K3L._AC_UF894,1000_QL80_.jpg";
+    }else if(img.src == "https://images-eu.ssl-images-amazon.com/images/I/51nrgs5yLIL._AC_UL600_SR600,600_.jpg"){
+        img.src = "https://media.product.which.co.uk/prod/images/original/eaa29035f160-ic17227-0882-00-rear-2000x1500.webp";
+    }else if(img.src == "https://media.product.which.co.uk/prod/images/original/eaa29035f160-ic17227-0882-00-rear-2000x1500.webp"){
+        img.src = "https://www.powerbuy.co.th/_next/image?url=https%3A%2F%2Fpim.powerbuy.co.th%2FPWBPIM%2Fweb%2FThumbnail%2FImage%2F1207%2F276670.jpg&w=640&q=75";
+    }else if(img.src == "https://www.powerbuy.co.th/_next/image?url=https%3A%2F%2Fpim.powerbuy.co.th%2FPWBPIM%2Fweb%2FThumbnail%2FImage%2F1207%2F276670.jpg&w=640&q=75"){
+        img.src = "https://isupplyfiji.com/cdn/shop/products/86431.1_1024x1024.webp?v=1682288793";
+    }else if(img.src == "https://isupplyfiji.com/cdn/shop/products/86431.1_1024x1024.webp?v=1682288793"){
+        img.src = "https://m.media-amazon.com/images/I/61+JCBk9tdL.jpg";
+    }else if(img.src == "https://m.media-amazon.com/images/I/61+JCBk9tdL.jpg"){
+        img.src = "https://www.discoazul.com/uploads/media/images/tablet-alcatel-3t10-2020-10-1-2gb-32gb-4g-negra-14.jpg";
+    }else if(img.src == "https://www.discoazul.com/uploads/media/images/tablet-alcatel-3t10-2020-10-1-2gb-32gb-4g-negra-14.jpg"){
+        img.src = "https://media.s-bol.com/qLZQZpqLRnRp/nRY9RXP/168x115.jpg";
+    }else if(img.src == "https://media.s-bol.com/qLZQZpqLRnRp/nRY9RXP/168x115.jpg"){
+        img.src = "https://image-us.samsung.com/SamsungUS/home/mobile/tablets/galaxy-tab-s7-fe/GalaxyTabS7plusLite_Combo_001_MysticGreen_1600x1200.png?$product-details-jpg$";
+    }else{
+        img.src = "https://robots.net/wp-content/uploads/2023/10/why-is-my-tablet-screen-black-1696681441.jpg";
+    }
+}
